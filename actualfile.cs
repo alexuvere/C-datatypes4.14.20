cb9d9c06a6bf5cff3d713c1bad53a904b53b68ad
Alex Uvere <alexuvere@gmail.com>
2020-04-14 11:22:36 -0500

    Create actualfile.cs

---
+++ actualfile.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CworkingWithStrings4._14._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+                            
+            Console.WriteLine(phrase.Substring(8, 3) );
+
+            Console.ReadLine();
+        }
+    }
+}
